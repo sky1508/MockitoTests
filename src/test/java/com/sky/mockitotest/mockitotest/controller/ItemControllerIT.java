af83572a60ea901bba7d054166d8459c2ee19b59
sky1508 <sky12345085@yahoo.com>
2020-03-15 14:13:34 +0530

    Sample Intg test

--- src/test/java/com/sky/mockitotest/mockitotest/controller/ItemControllerIT.java
+++ src/test/java/com/sky/mockitotest/mockitotest/controller/ItemControllerIT.java
@@ -1,6 +1,5 @@
 package com.sky.mockitotest.mockitotest.controller;
 
-import com.sky.mockitotest.mockitotest.data.ItemRepository;
 import com.sky.mockitotest.mockitotest.spike.JsonAssertTest;
 import org.json.JSONException;
 import org.junit.Test;
@@ -8,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,7 +19,6 @@ public class ItemControllerIT {
 
     @Test
     public void contextLoads() throws JSONException {
-
         String response = this.testRestTemplate.getForObject("/all-item-from-database", String.class);
 
         JSONAssert.assertEquals("[{id:1},{id:2},{id:3}]",response,false);
