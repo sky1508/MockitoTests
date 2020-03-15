11afe09013956d10d56b4c1f6c7a93c90ce88cea
sky1508 <sky12345085@yahoo.com>
2020-03-15 14:10:15 +0530

    Basic ctrller testing

--- src/test/java/com/sky/mockitotest/mockitotest/ListMockTest.java
+++ src/test/java/com/sky/mockitotest/mockitotest/ListMockTest.java
@@ -6,6 +6,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -84,5 +85,38 @@ public class ListMockTest {
         List<String> allValues = captor.getAllValues();
         assertEquals("test", allValues.get(0));
         assertEquals("test2", allValues.get(1));
+    }
+
+    @Test
+    public void mocking() {
+        ArrayList arrayListMock = mock(ArrayList.class);
+        System.out.println(arrayListMock.get(0));
+        System.out.println(arrayListMock.size());
+        arrayListMock.add("Test");
+        arrayListMock.add("Test2");
+        System.out.println(arrayListMock.size());
+
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println(arrayListMock.size());
+
+    }
+
+    @Test
+    public void spying() {
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("Test0 ");
+        System.out.println(arrayListSpy.get(0));
+        System.out.println(arrayListSpy.size());
+        arrayListSpy.add("Test1");
+        arrayListSpy.add("Test2");
+        System.out.println(arrayListSpy.size());
+
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println(arrayListSpy.size());
+
+        arrayListSpy.add("Test3");
+        System.out.println(arrayListSpy.size());
+
+        verify(arrayListSpy).add("Test3");
     }
 }
