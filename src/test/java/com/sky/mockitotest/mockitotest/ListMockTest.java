06742b91a9c8e3ef8d6124e3c8206a2b730cdcec
sky1508 <sky12345085@yahoo.com>
2020-03-15 14:07:50 +0530

    testing lists

--- src/test/java/com/sky/mockitotest/mockitotest/ListMockTest.java
+++ src/test/java/com/sky/mockitotest/mockitotest/ListMockTest.java
@@ -2,22 +2,20 @@ package com.sky.mockitotest.mockitotest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 //@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class ListMockTest {
 
-    List<String> mock = mock(List.class);
+    List mock = mock(List.class);
 
     @Test
     public void size_basic() {
@@ -37,86 +35,5 @@ public class ListMockTest {
         when(mock.get(0)).thenReturn("parm");
         assertEquals("parm", mock.get(0));
         assertEquals(null, mock.get(1));
-    }
-
-    @Test
-    public void returnWithGenericParameters() {
-        when(mock.get(anyInt())).thenReturn("parm");
-        assertEquals("parm", mock.get(0));
-        assertEquals("parm", mock.get(1));
-    }
-
-    @Test
-    public void verificationBasics() {
-        //SUT
-        String value1 = mock.get(0);
-        String value2 = mock.get(1);
-
-        //verify
-        verify(mock).get(0);
-        verify(mock, times(2)).get(anyInt());
-        verify(mock, atLeastOnce()).get(anyInt());
-        verify(mock, atMost(2)).get(anyInt());
-        verify(mock, never()).get(2);
-    }
-
-    @Test
-    public void argumentCapturing() {
-        //SUT
-        mock.add("test");
-
-        //verify
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(mock).add(captor.capture());
-
-        assertEquals("test", captor.getValue());
-    }
-
-    @Test
-    public void multiArgumentCapturing() {
-        //SUT
-        mock.add("test");
-        mock.add("test2");
-
-        //verify
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(mock, times(2)).add(captor.capture());
-
-        List<String> allValues = captor.getAllValues();
-        assertEquals("test", allValues.get(0));
-        assertEquals("test2", allValues.get(1));
-    }
-
-    @Test
-    public void mocking() {
-        ArrayList arrayListMock = mock(ArrayList.class);
-        System.out.println(arrayListMock.get(0));
-        System.out.println(arrayListMock.size());
-        arrayListMock.add("Test");
-        arrayListMock.add("Test2");
-        System.out.println(arrayListMock.size());
-
-        when(arrayListMock.size()).thenReturn(5);
-        System.out.println(arrayListMock.size());
-
-    }
-
-    @Test
-    public void spying() {
-        ArrayList arrayListSpy = spy(ArrayList.class);
-        arrayListSpy.add("Test0 ");
-        System.out.println(arrayListSpy.get(0));
-        System.out.println(arrayListSpy.size());
-        arrayListSpy.add("Test1");
-        arrayListSpy.add("Test2");
-        System.out.println(arrayListSpy.size());
-
-        when(arrayListSpy.size()).thenReturn(5);
-        System.out.println(arrayListSpy.size());
-
-        arrayListSpy.add("Test3");
-        System.out.println(arrayListSpy.size());
-
-        verify(arrayListSpy).add("Test3");
     }
 }
