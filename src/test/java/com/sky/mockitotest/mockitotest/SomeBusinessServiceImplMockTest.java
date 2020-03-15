91b6cc0b13c9b5da4322df45c0b2dae48ebfd41f
sky1508 <sky12345085@yahoo.com>
2020-03-15 14:07:02 +0530

    injectmocks

--- src/test/java/com/sky/mockitotest/mockitotest/SomeBusinessServiceImplMockTest.java
+++ src/test/java/com/sky/mockitotest/mockitotest/SomeBusinessServiceImplMockTest.java
@@ -2,45 +2,44 @@ package com.sky.mockitotest.mockitotest;
 
 import com.sky.mockitotest.mockitotest.data.SomeDataService;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class SomeBusinessServiceImplMockTest {
 
-    SomeBusinessServiceImpl sb = new SomeBusinessServiceImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
+    @InjectMocks
+    SomeBusinessServiceImpl someBusinessService;
 
-    @Before
-    public void before() {
-        sb.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_basic() {
 
         //dataServiceMock retrievAll new int[] {1, 2, 3}
         when(dataServiceMock.retrievAll()).thenReturn(new int[] {1,2,3});
-        Assert.assertEquals(6, sb.calculateSumUsingDataService());
+        Assert.assertEquals(6, someBusinessService.calculateSumUsingDataService());
     }
 
     @Test
     public void calculateSum_empty() {
         when(dataServiceMock.retrievAll()).thenReturn(new int[] {});
-        Assert.assertEquals(0, sb.calculateSumUsingDataService());
+        Assert.assertEquals(0, someBusinessService.calculateSumUsingDataService());
     }
 
     @Test
     public void calculateSum_one() {
         when(dataServiceMock.retrievAll()).thenReturn(new int[] {1});
-        Assert.assertEquals(1, sb.calculateSumUsingDataService());
+        Assert.assertEquals(1, someBusinessService.calculateSumUsingDataService());
     }
 }
